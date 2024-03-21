0ee94fe902d6ab27d49934d2b846770203c49a00
Joao <joaoguidev@gmail.com>
2024-03-20 19:42:27 -0700

    try 3

--- app/root.jsx
+++ app/root.jsx
@@ -12,7 +12,7 @@ export function Layout({ children }) {
    return (
       <html className="dark size-full" lang="en">
          <head>
-            <meta charSet="utf-8" />
+            <meta charSet="utf-8 " />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <Meta />
             <Links />
