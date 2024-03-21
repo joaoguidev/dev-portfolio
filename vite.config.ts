03ff952ad3de13657f1f0863228d5d869e991c8d
Joao <joaoguidev@gmail.com>
2024-03-20 17:29:24 -0700

    added installGlobals on Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import {
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals()
 
 export default defineConfig({
   plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
