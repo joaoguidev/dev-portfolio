c3f4fa1bc64b3bb33a858e736a0ebc7db9bc7dd4
Joao <joaoguidev@gmail.com>
2024-03-23 13:14:26 -0700

    fixed error with stars

--- app/components/canvas/Stars.jsx
+++ app/components/canvas/Stars.jsx
@@ -5,7 +5,7 @@ import { Suspense, useRef, useState } from "react"
 
 const Stars = (props) => {
    const ref = useRef()
-   const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }))
+   const [sphere] = useState(() => random.inSphere(new Float32Array(5001), { radius: 1.2 }))
 
    useFrame((state, delta) => {
       ref.current.rotation.x -= delta / 20
