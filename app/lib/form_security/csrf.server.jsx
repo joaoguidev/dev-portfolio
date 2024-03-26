4379d59177429beda21a0794a7c3657428d55ad3
Joao <joaoguidev@gmail.com>
2024-03-26 11:22:09 -0700

    fixed csp style-src

--- app/lib/form_security/csrf.server.jsx
+++ app/lib/form_security/csrf.server.jsx
@@ -2,12 +2,12 @@ import { createCookie } from "@remix-run/cloudflare"
 import { CSRF } from "remix-utils/csrf/server";
 // import { createCookie } from "@remix-run/node"; // or cloudflare/deno
 
-//TODO - Bring the secret correctly from the .dev.vars. How to make this available here without context?
 export const cookie = createCookie("csrf", {
 	path: "/",
 	httpOnly: true,
 	secure: true,
 	sameSite: "lax",
+	//TODO - Bring the secret correctly from the .dev.vars. How to make this available here without context?
 	secrets: ['4pTEL7hv1ebsLbtwc3NjfQuywCz5CerxPbmwhfC7vnWBqsjr0vBYTNT7GAe9cAtrPeU='],
 });
 
@@ -17,6 +17,7 @@ export const csrf = new CSRF({
 	// what key in FormData objects will be used for the token, defaults to `csrf`
 	formDataKey: "csrf",
 	// an optional secret used to sign the token, recommended for extra safety
+	//TODO - Bring the secret correctly from the .dev.vars. How to make this available here without context?
 	secret: '4pTEL7hv1ebsLbtwc3NjfQuywCz5CerxPbmwhfC7vnWBqsjr0vBYTNT7GAe9cAtrPeU=',
 });
 
